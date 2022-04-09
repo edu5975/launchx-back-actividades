cb36cfb820eeb7dc29e56858c7d6a36212abc090
eduardo <eduardo.rico@cidecuc.mx>
2022-04-09 09:19:03 -0500

    actividades

---
+++ weekly_mission_1/example10/pokemon.js
@@ -0,0 +1,14 @@
+// exports a class
+export default class Pokemon {
+    constructor (name) {
+      this.name = name
+    }
+  
+    sayHello () {
+        console.log(`Mi pokemon ${this.name} te saluda`)
+    }
+
+    sayMessage (message) {
+        console.log(`Mi pokemon ${this.name} te dice: ${message}`)
+    }
+}
