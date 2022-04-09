dfbecd63cbd9af6954f5307d2f9d202cf5f8629f
eduardo <eduardo.rico@cidecuc.mx>
2022-04-09 09:19:17 -0500

    actividades

---
+++ weekly_mission_1/example9/pokemon.js
@@ -0,0 +1,17 @@
+// exports a class
+class Pokemon {
+    constructor (name) {
+      this.name = name
+    }
+  
+    sayHello () {
+        console.log(`Mi pokemon ${this.name} te saluda`)
+    }
+
+    sayMessage (message) {
+        console.log(`Mi pokemon ${this.name} te dice: ${message}`)
+    }
+}
+
+// Esta clase se exporta en este módulo
+module.exports = Pokemon
